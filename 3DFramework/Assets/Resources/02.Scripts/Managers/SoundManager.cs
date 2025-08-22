adb388977c521c4c4188e3a2afb9787002115121
jinjin7576 <jinjin7576@daum.net>
2025-08-22 13:14:00 +0900

    2
    2

--- 3DFramework/Assets/Resources/02.Scripts/Managers/SoundManager.cs
+++ 3DFramework/Assets/Resources/02.Scripts/Managers/SoundManager.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class SoundManager
 {
+    AudioSource[] _audioSources = new AudioSource[(int)Define.Sound.MaxCount];
     public void Play(Define.Sound type, string path, float pitch = 1.0f)
     {
+        if (path.Contains("05.Sound/") == false)
+        {
+            path = $"05.Sound/{path}";
+        }
+        if (type == Define.Sound.Bgm)
+        {
+            AudioClip audioClip = Managers.Resource.Load<AudioClip>(path);
+            if (audioClip == null)
+            {
+                Debug.Log($"AudioClip Missing! {path}");
+                return;
+            }
+        }
+        else
+        {
 
+        }
     }
 }
