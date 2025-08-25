88d94132444c0a393626652284b701f6a0e8b345
jinjin7576 <jinjin7576@daum.net>
2025-08-25 10:32:15 +0900

    2
    2

--- 3DFramework/Assets/Resources/02.Scripts/Managers/PoolManager.cs
+++ 3DFramework/Assets/Resources/02.Scripts/Managers/PoolManager.cs
@@ -1,6 +1,81 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolManager
 {
-    
+    class Pool
+    {
+        public GameObject Original { get; private set; }
+        public Transform Root { get; set; }
+        Stack<Poolable> _poolStack = new Stack<Poolable>();
+
+        public void Init(GameObject original, int count = 5)
+        {
+            Original = original;
+            Root = new GameObject().transform;
+            Root.name = $"{original.name} _Root";
+
+            for (int i = 0; i < count; i++)
+            {
+                Push(Create());
+            }
+        }
+
+        private Poolable Create()
+        {
+            GameObject go = Object.Instantiate<GameObject>(Original);
+            go.name = Original.name; //(clone)이 나오지 않도록 처리
+            return go.GetorAddComponent<Poolable>();
+        }
+
+        public void Push(Poolable poolable)
+        {
+            if (poolable == null) return;
+
+            poolable.transform.parent = Root;
+            poolable.gameObject.SetActive(false);
+            poolable.IsUsing = false;
+
+            _poolStack.Push(poolable);
+        }
+        public Poolable Pop(Transform parent)
+        {
+            Poolable poolable;
+            if(_poolStack.Count > 0)
+            {
+                poolable = _poolStack.Pop();
+            }
+            else
+            {
+                poolable = Create();
+            }
+            poolable.gameObject.SetActive(true);
+            poolable.transform.parent = parent;
+            poolable.IsUsing = true;
+            return poolable;
+        }
+    }
+    //각각 풀들은 키를 이용해 이름을 이용해서 관리하기
+    Dictionary<string, Pool> _pool = new Dictionary<string, Pool>();
+
+    Transform _root;
+    public void Init()
+    {
+        if (_root == null)
+        {
+            _root = new GameObject { name = "@Pool_Root" }.transform;
+            Object.DontDestroyOnLoad(_root);
+        }
+
+    }
+
+
+    public Poolable Pop(GameObject original, Transform parent = null)
+    {
+        return null;
+    }
+    public GameObject GetOrigianl(string name)
+    {
+        return null;
+    }
 }
--- 3DFramework/Assets/Resources/02.Scripts/Managers/ResourceManager.cs
+++ 3DFramework/Assets/Resources/02.Scripts/Managers/ResourceManager.cs
@@ -11,22 +11,24 @@ public class ResourceManager
 
     public GameObject Instatiate(string path, Transform parent = null)
     {
+        //1. 오리지날이 있으면 바로 사용, 없으면 아래처럼
         GameObject prefab = Load<GameObject>($"03.Prefabs/{path}");
         if (prefab == null)
         {
             Debug.Log($"Failed to load prefab : {path}");
             return null;
         }
-
+        //2. 혹시 풀링된 오브젝트가 있다면 그것을 반환
         GameObject go = Object.Instantiate(prefab, parent);
-        int index = go.name.IndexOf("(Clone)"); // UI_Inven_Item(Clone) -> 원래이름 
+        int index = go.name.IndexOf("(Clone)");
         if (index > 0)
         {
-            go.name = go.name.Substring(0, index); //UI_Inven_Item -> 추출한 이름 / (Clone) -> 뽑아낸 이름
+            go.name = go.name.Substring(0, index);
         }
 
         return go;
     }
+    //3. 만약 풀링이 필요한 오브젝트라면 바로 삭제x 풀링매니저한테 위탁
     public void Destroy(GameObject go)
     {
         if (go == null)
